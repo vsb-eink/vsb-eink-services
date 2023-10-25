505ce1405f4b81182cc2abec31f59927f8eecadd
Marek Lukáš <tajnymag@gmail.com>
2023-10-25 15:29:14 +0200

    Handle errors during rendering and dispose browser accordingly

--- packages/renderer/src/vsb-eink-renderer.ts
+++ packages/renderer/src/vsb-eink-renderer.ts
@@ -3,7 +3,7 @@
 import { pathToFileURL } from 'node:url';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers'
-import { chromium } from 'playwright';
+import { Browser, chromium } from 'playwright';
 import { parse as parseRSS } from 'rss-to-json';
 
 async function main() {
@@ -35,29 +35,34 @@ async function main() {
         .help()
         .parseSync();
 
-    const browser = await chromium.launch({
-        args: [
-            '--disable-lcd-text',
-            '--disable-font-subpixel-positioning'
-        ]
-    });
+	let browser: Browser | null = null;
+	try {
+		browser = await chromium.launch({
+			args: [
+				'--disable-lcd-text',
+				'--disable-font-subpixel-positioning'
+			]
+		});
 
-    const context = await browser.newContext({
-        deviceScaleFactor: 2
-    });
+		const context = await browser.newContext({
+			deviceScaleFactor: 2
+		});
 
-    await context.route('http://vsb-eink-renderer/rss', async route => {
-        const rss = await parseRSS('https://info.sso.vsb.cz/cz.vsb.edison.info.web/rss?orgUnitId=1');
-        return route.fulfill({status: 200, json: rss});
-    });
-    context.on('console', msg => console.log(msg.text()));
+		await context.route('http://vsb-eink-renderer/rss', async route => {
+			const rss = await parseRSS('https://info.sso.vsb.cz/cz.vsb.edison.info.web/rss?orgUnitId=1');
+			return route.fulfill({status: 200, json: rss});
+		});
+		context.on('console', msg => console.log(msg.text()));
 
-    const page = await context.newPage();
-    await page.setViewportSize({width: options.width, height: options.height});
+		const page = await context.newPage();
+		await page.setViewportSize({width: options.width, height: options.height});
 
-    await page.goto(pathToFileURL(options.path).href, { waitUntil: 'networkidle' });
-    await page.screenshot({path: options.output});
-
-    await browser.close();
+		await page.goto(pathToFileURL(options.path).href, { waitUntil: 'networkidle' });
+		await page.screenshot({path: options.output});
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await browser?.close();
+	}
 }
 main().catch(console.error);
