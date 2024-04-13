af6a3da373db1cf83e6bba7dce2d9cec1c86f16c
Marek Lukáš <tajnymag@gmail.com>
2024-04-13 21:20:11 +0200

    Actually allow import-meta-env to fail

--- apps/dashboard-vue/nginx/docker-entrypoint.d/import-meta-env.sh
+++ apps/dashboard-vue/nginx/docker-entrypoint.d/import-meta-env.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/env/import-meta-env --disposable --example /opt/env/.env.example --path /usr/share/nginx/html/index.html
+/opt/env/import-meta-env --disposable --example /opt/env/.env.example --path /usr/share/nginx/html/index.html || exit 0
