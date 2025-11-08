c906df9a0b64dfd63da8da8498b2b9c3e173943f
tajnymag <tajnymag@gmail.com>
2025-11-08 15:07:31 +0100

    Renderer: retry fetching of mimetype if server doesn't support HEAD method

--- services/renderer/src/utils/http.ts
+++ services/renderer/src/utils/http.ts
@@ -1,4 +1,6 @@
 export async function fetchMimeType(url: string): Promise<string> {
-	const response = await fetch(url, { method: 'HEAD' });
+	const response = await fetch(url, { method: 'HEAD' }).then((res) => {
+		return res.status === 405 ? fetch(url, { method: 'GET' }) : res;
+	});
 	return response.headers.get('content-type') || '';
 }
